d858312b9284321cfddaf7b149626106eb511f84
Hariharaan S <s.hariharaan.college@gmail.com>
2023-07-27 18:58:06 +0530

    Commit changes

---
+++ Decompress Run-Length Encoded List/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] decompressRLElist(int[] nums) {
+        List<Integer>res1 = new ArrayList<>();
+        for(int i=0;i<nums.length;i+=2){
+            int freq = nums[i];
+            int val = nums[i+1];
+            for(int j=0;j<freq;j++){
+                res1.add(val);
+            }
+        }
+        int res[] = new int[res1.size()];
+        for(int i=0;i<res1.size();i++){
+            res[i] = res1.get(i);
+        }
+        return res;
+    }
+}
