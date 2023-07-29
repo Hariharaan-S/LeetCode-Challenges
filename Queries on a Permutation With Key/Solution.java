d102989cb4f4077651e084dd27600aa67152f951
Hariharaan S <s.hariharaan.college@gmail.com>
2023-07-30 00:02:57 +0530

    Solution to LeetCode Challenge 1409

---
+++ Queries on a Permutation With Key/Solution.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] processQueries(int[] queries, int m) {
+        List<Integer> p = new ArrayList<>();
+        for(int i=0;i<m;i++){
+            p.add(i+1);
+        }
+        int []res = new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+            res[i] = p.indexOf(queries[i]);
+            p.remove(p.indexOf(queries[i]));
+            p.add(0,queries[i]);
+        }
+        return res;
+    }
+}
