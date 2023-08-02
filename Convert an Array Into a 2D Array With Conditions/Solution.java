5a644a5eb1ad4d4447af78594a152c0896ce2aa1
Hariharaan S <s.hariharaan.college@gmail.com>
2023-08-02 22:46:07 +0530

    Solution to LeetCode Challenge 2610

---
+++ Convert an Array Into a 2D Array With Conditions/Solution.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+
+    private static int ifAny(HashMap<Integer,Integer>map){
+        for(int i:map.keySet()){
+            if(map.get(i)>=1) return 1;
+        }
+        return 0;
+    }
+
+    public List<List<Integer>> findMatrix(int[] nums) {
+        HashMap<Integer,Integer>map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int temp = map.getOrDefault(nums[i],0);
+            map.put(nums[i],temp+1);
+        }
+        List<Integer>temp = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        while(ifAny(map) == 1){
+        for(int i:map.keySet()){
+            if(map.get(i)>=1){
+                temp.add(i);
+                map.put(i,map.get(i)-1);
+            }
+        }
+        res.add(new ArrayList<>(temp));
+        temp.clear();
+        }
+        return res;
+    }
+}
