40705f7209872aa2e34d8ae515a5eba77aefa6fd
Hariharaan S <s.hariharaan.college@gmail.com>
2023-08-02 23:09:37 +0530

    Solution to LeetCode Challenge 2161

---
+++ Partition Array According to Given Pivot/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int res[] = new int[nums.length];
+        int left = 0;
+        int right = nums.length-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<pivot){
+                res[left++] = nums[i];
+            }
+            if(nums[nums.length-1-i]>pivot){
+                res[right--] = nums[nums.length-1-i];
+            }
+        }
+        while(left<=right){
+            res[left++] = pivot;
+        }
+        return res;
+    }
+}
