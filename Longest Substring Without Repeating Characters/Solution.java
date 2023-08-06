9cc9e12ecba564a4390e11e665acf62e531cdac1
Hariharaan S <s.hariharaan2003@gmail.com>
2023-08-06 18:18:59 +0530

    Solution to Leetcode Challenges

--- Longest Substring Without Repeating Characters/Solution.java
+++ Longest Substring Without Repeating Characters/Solution.java
@@ -1,56 +1,28 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        int i = 0;
-        int j = 0;
-        int max = 0;
-        while(j < s.length()){
-            map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1);
-            if(map.size() == j - i + 1){
-                max = Math.max(max, j - i + 1);
-                j++;
-            }
-            else if(map.size() < j - i + 1){
-                while(map.size() < j - i + 1){
-                    map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
-                    if(map.get(s.charAt(i)) == 0) map.remove(s.charAt(i));
-                    i++;
+        Set<Character>set=new HashSet<>();
+        int maxLength=0;
+        int left=0;
+        for(int right=0;right<s.length();right++){
+           
+            if(!set.contains(s.charAt(right))){
+                set.add(s.charAt(right));
+                maxLength=Math.max(maxLength,right-left+1);
+                
+            }else{
+                while(s.charAt(left)!=s.charAt(right)){
+                    set.remove(s.charAt(left));
+                    left++;
                 }
-                j++;
+                set.remove(s.charAt(left));left++;
+                set.add(s.charAt(right));
             }
+            
         }
-        return max;
+        return maxLength;
     }
 }
 
-// Sliding Window Template
-// while(j < size()){
-
-//     // Calculation's happen's here
-// -----------------------------------------------
-//     if(condition < k){
-//         j++;
-//     }
-// -----------------------------------------------
-
-// -----------------------------------------------
-//     else if(condition == k){
-//         // ans <-- calculation
-//         j++;
-//     }
-// ----------------------------------------------
-
-// ----------------------------------------------
-//     else if(condition > k){
-//         while(condition > k){
-//             // remove calculation for i
-//             i++;
-//         }
-//         j++;
-//     }
-// ----------------------------------------------
-// }
-// return ans;
